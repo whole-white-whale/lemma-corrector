c6bfa6971e047e1819bc14c7e21ffda7e82c6043
Sergey Kirichenko <whole.white.whale@gmail.com>
2026-07-09 19:54:17 +0400

    Get leaders of the communities

--- src/lemma_corrector/__init__.py
+++ src/lemma_corrector/__init__.py
@@ -17,8 +17,15 @@ class LemmaCorrector:
     def get_graph(self) -> nx.Graph:
         graph = nx.Graph()
 
+        if len(self.bag_of_lemmata) == 0:
+            return graph
+
+        max_lemma_frequency = self.bag_of_lemmata.most_common(1)[0][1]
+
         for lemma in self.bag_of_lemmata:
-            graph.add_node(lemma)
+            graph.add_node(
+                lemma, weight=self.bag_of_lemmata[lemma] / max_lemma_frequency
+            )
 
         for node_1, node_2 in combinations(graph.nodes, 2):
             if DamerauLevenshtein.distance(node_1, node_2) == 1:
@@ -28,3 +35,23 @@ class LemmaCorrector:
 
     def get_communities(self, graph: nx.Graph) -> list[set[str]]:
         return nx.community.louvain_communities(graph, threshold=self.epsilon)
+
+    def get_leader(self, graph: nx.Graph, community: set[str]) -> str | None:
+        leader_by_node_weight = self.get_leader_by_node_weight(graph, community)
+        leader_by_rank = self.get_leader_by_rank(graph, community)
+
+        if leader_by_node_weight != leader_by_rank:
+            return None
+
+        return leader_by_rank
+
+    def get_leader_by_node_weight(self, graph: nx.Graph, community: set[str]) -> str:
+        return max(
+            graph.subgraph(community).nodes.items(), key=lambda x: x[1]["weight"]
+        )[0]
+
+    def get_leader_by_rank(self, graph: nx.Graph, community: set[str]) -> str:
+        return max(
+            nx.pagerank(graph.subgraph(community), tol=self.epsilon).items(),
+            key=lambda x: x[1],
+        )[0]
